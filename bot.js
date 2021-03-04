07391fa8811dcc4789500cee3a90a6d35bbce427
ElectronPro <71539127+ElectronPro@users.noreply.github.com>
2021-03-04 16:04:22 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,9 @@
+const Discord = require('discord.js');
 
+const client = new Discord.Client();
+
+const token = ('ODE2OTc0MzYwNzgyODMxNjM2.YECw2A._n0vCslfBDhAcVoiZCDxWuYQWj4')
+
+
+client.on('ready', () =>{
+      console.log("ready for action!");
